c2bfd87f9ddf9d26757e74b5526cec215889d6cc
José Arthur <josearthur87@outlook.com>
2024-10-01 23:33:51 -0300

    adição do touchscreen

--- scripts.js
+++ scripts.js
@@ -5,23 +5,19 @@ const pontuacao = document.getElementById("pontuacao");
 const record = document.getElementById("record");
 const atualizar = document.getElementById("atualizar");
 
-
 var audio = new Audio('./sons/dubstep-sonic-canyons-13123.mp3');
 audio.loop = true;
 window.onload = function() {
     audio.play();
 };
 
-
 var collisionSound = new Audio('./sons/videogame-death-sound-43894.mp3');
 
-
-
-let pontuacaoAtual = 0
+let pontuacaoAtual = 0;
 let pontuacaoMaxima = localStorage.getItem("maxScore") || 0;
 
-const numberToString = parseInt(pontuacaoMaxima)
-record.textContent = "Recorde: " + numberToString.toFixed(0)
+const numberToString = parseInt(pontuacaoMaxima);
+record.textContent = "Recorde: " + numberToString.toFixed(0);
 
 function updateScore(pontos) {
   if (pontos > pontuacaoMaxima) {
@@ -31,26 +27,33 @@ function updateScore(pontos) {
 }
 
 function jump () {
-  if (sonic.classList != "jump"){
+  if (sonic.classList != "jump") {
     sonic.classList.add("jump");
     
     setTimeout(function () {
       sonic.classList.remove("jump");
     }, 500);
   }
-} 
+}
+
+document.addEventListener("keydown", function (event) {
+  jump();
+});
+
+document.addEventListener("touchstart", function (event) {
+  jump();
+});
 
 let loop = setInterval(function () {
-
   const bombPosition = parseInt(window.getComputedStyle(bomb).getPropertyValue("left"));
   const sonicPosition = parseInt(window.getComputedStyle(sonic).getPropertyValue("bottom"));
   pontuacaoAtual = pontuacaoAtual + 0.09;
   const info = "Pontuação: " + pontuacaoAtual.toFixed(1); 
-  pontuacao.textContent = info
+  pontuacao.textContent = info;
     
   if (bombPosition <= 120 && bombPosition > 0 && sonicPosition < 80) {
     audio.pause();
-    collisionSound.play()
+    collisionSound.play();
     bomb.style.animation = "none";
     bomb.style.left = `${bombPosition}px`;
     
@@ -60,8 +63,8 @@ let loop = setInterval(function () {
     sonic.style.width = '120px';
     sonic.style.marginLeft = "40px";
     
-    gameover.style.marginLeft = "35%"
-    atualizar.style.marginLeft = "48%"
+    gameover.style.marginLeft = "35%";
+    atualizar.style.marginLeft = "48%";
 
     updateScore(pontuacaoAtual);
 
@@ -69,9 +72,3 @@ let loop = setInterval(function () {
   }
 
 }, 10);
-
-document.addEventListener("keydown", function (event) {
-  jump()
-});
-
-
